85e6b4d961beed159732e186ec4d5846ce9dcafa
wsnx <wisnuxmurti@gmail.com>
2017-08-24 21:46:56 +0700

    Update

--- app/src/main/java/id/my/wisnumurti/wisnuqrcodescanner/MainActivity.java
+++ app/src/main/java/id/my/wisnumurti/wisnuqrcodescanner/MainActivity.java
@@ -8,12 +8,10 @@ import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telecom.Call;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RemoteViews;
 import android.widget.TextView;
 import android.widget.Toast;
 
